d7a3f0fe69954509ab0211670d233f8c388d01a8
AbeloByte <gobeztech20@gmail.com>
2025-08-19 00:23:40 +0300

    initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home.jsx";
+import Menu from "./features/menu/Menu.jsx";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
